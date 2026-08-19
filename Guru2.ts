d5069ce6932dc9502930f5ba4400993077013ee6
muhammad yusuf gibran X RPL 6 <muhammad_gibran_35rpl@student.smktelkom-mlg.sch.id>
2026-08-19 10:00:07 +0700

    Add files via upload

---
+++ Guru2.ts
@@ -0,0 +1,14 @@
+const guru : {
+    nama: String;
+    umur: Number;
+    sertifikasi: Boolean;
+    mapel: String;
+    telepon: String
+} = {
+    nama: "Dimas",
+    umur: 40,
+    sertifikasi: true,
+    mapel: "PJOK",
+    telepon: "08123456789"
+}
+console.log(guru)
